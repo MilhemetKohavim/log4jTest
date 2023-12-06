f999ceb8376e99097cc206c4b3e0c180a350fcc0
MilhemetKohavim <34343844+MilhemetKohavim@users.noreply.github.com>
2023-12-06 11:38:21 -0500

    Add files via upload

---
+++ viks.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Example {
+    public static void main(String[] args) {
+        String input = "example input";
+        String output = StringUtils.upperCase(input);
+        System.out.println(output);
+    }
+}
